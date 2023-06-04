7efbb127cc3a4febfbfce1b6865bab91929133dd
hotaek0603 <128003475+hotaek0603@users.noreply.github.com>
2023-06-04 23:07:05 +0900

    Add files via upload
    implement hw5

--- figure_main.py
+++ figure_main.py
@@ -1,11 +1,16 @@
-import figure
+# %%
+
+# [fill this line]
+
 myline = figure.line(10)
+
 square = figure.area_square(myline.get_length())
 print(square)
+
 myline.set_length(20)
-regular_triangle =
-figure.area_regular_triangle(myline.get_length())
+regular_triangle = figure.area_regular_triangle(myline.get_length())
 print(regular_triangle)
-myline.set_length（30）
+
+myline.set_length(30)
 circle = figure.area_circle(myline.get_length())
 print(circle)
